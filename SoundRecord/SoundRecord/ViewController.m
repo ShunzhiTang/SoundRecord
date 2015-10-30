9c9da835f7351fcf6d0fd41ed477bc972a1ad026
tangShunZhi <1046975221@qq.com>
2015-10-30 13:31:36 -0700

    1、完成录音的存储 2、如何把一个NSString 转化为NSURL ，如果加上 协议头

--- SoundRecord/SoundRecord/ViewController.m
+++ SoundRecord/SoundRecord/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
+#import <AVFoundation/AVFoundation.h>
+
 @interface ViewController ()
+
+//声明一个全局的 录音
+@property (nonatomic , strong)AVAudioRecorder *recoeder;
 
 @end
 
@@ -19,9 +24,27 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    [self.recoeder record];
 }
 
+#pragma mark 把录音的懒加载出来 , 保存录音
+- (AVAudioRecorder *)recoeder{
+    
+    if (_recoeder == nil) {
+        //1、获取沙盒
+        NSString *url  = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+      
+        NSString *filePath = [url stringByAppendingPathComponent:@"abc.caf"];
+        //2、需要加上文件协议  ,需要把NSString 转化为nsurl
+        
+        
+        NSURL *fileUrl = [NSURL fileURLWithPath:filePath];
+        
+        //创建录音对象
+        _recoeder = [[AVAudioRecorder alloc] initWithURL:fileUrl settings:nil error:nil];
+    }
+    return _recoeder;
+}
 @end
